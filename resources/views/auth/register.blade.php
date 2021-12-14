fe7031d0af0f3a2ce658c744e1717bccc9d24668
lavertis <29688908+Lavertis@users.noreply.github.com>
2021-12-14 19:37:08 +0100

    fix: if and elseif instead of if and error directive for is-valid and is-invalid

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,14 +2,16 @@
 
 @section('content')
     <div class="container container px-4 px-lg-5 my-auto h-100">
-        <div class="col-10 col-md-8 col-lg-4 mx-auto bg-light rounded-3 p-5 shadow">
+        <div class="col-11 col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-auto bg-light rounded-3 p-5 shadow">
             <form action="{{ route('register') }}" method="post" class="needs-validation" novalidate>
                 @csrf
                 <div class="mb-3">
                     <label for="username" class="form-label">Username</label>
                     <input type="text" name="username" id="username" value="{{ old('username') }}"
-                           class="form-control @if ($errors->any()) is-valid @endif
-                           @error('username') is-invalid @enderror">
+                           class="form-control
+                                @if ($errors->any() && !$errors->get('username')) is-valid
+                                @elseif($errors->get('username')) is-invalid
+                                @endif">
                     @error('username')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -17,8 +19,10 @@
                 <div class="mb-3">
                     <label for="email" class="form-label">Email address</label>
                     <input type="email" name="email" id="email" value="{{ old('email') }}"
-                           class="form-control @if ($errors->any()) is-valid @endif
-                           @error('email') is-invalid @enderror">
+                           class="form-control
+                                    @if ($errors->any() && !$errors->get('email')) is-valid
+                                    @elseif($errors->get('email')) is-invalid
+                                    @endif">
                     @error('email')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -26,18 +30,17 @@
                 <div class="mb-3">
                     <label for="password" class="form-label">Password</label>
                     <input type="password" name="password" id="password"
-                           class="form-control @error('password') is-invalid @enderror">
+                           class="form-control
+                                    @if ($errors->any() && !$errors->get('password')) is-valid
+                                    @elseif($errors->get('password')) is-invalid
+                                    @endif">
                     @error('password')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="mb-3">
                     <label for="password_confirmation" class="form-label">Confirm password</label>
-                    <input type="password" name="password_confirmation" id="password_confirmation"
-                           class="form-control @error('password_confirmation') is-invalid @enderror">
-                    @error('password_confirmation')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
+                    <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
                 </div>
                 <div class="d-grid">
                     <button type="submit" class="btn btn-primary">Register</button>
